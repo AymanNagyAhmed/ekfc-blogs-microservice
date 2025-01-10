78f902f1afb0bd25aec65a4eb7a7c780fb938898
AymanNagyAhmed <AymanNagy.Ahmed@gmail.com>
2025-01-10 19:14:39 +0400

    - Add  Global prefix - Use GlobalExceptionFilter - Use AuthGuard - Use swagger Docs - Add helth check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from '@/modules/posts/posts.module';
+import { DatabaseModule } from '@/config/database/database.module';
+import { validationSchema } from '@/config/env.validation';
+import { RmqModule } from '@/config/rmq/rmq.module';
+import { BLOGS_SERVICE } from '@/common/constants/services';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema
+    }),
+    DatabaseModule,
+    PostsModule,
+    RmqModule.register({ name: BLOGS_SERVICE }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
